4e87185c1df0ab8597997def6526b845c6fc997b
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2023-05-10 10:07:18 -0300

    fix: pathParams in doc and config at sendEmail

--- src/utils/sendEmail.utils.ts
+++ src/utils/sendEmail.utils.ts
@@ -10,7 +10,7 @@ const sendEmailService = async ({
 }: iSendEmailRequest): Promise<String> => {
 	const tranporter = createTransport({
 		host: process.env.SMTP_HOST,
-		port: Number(process.env.SMTP_PORT) || undefined,
+		port: process.env.SMTP_PORT ? Number(process.env.SMTP_PORT) : undefined,
 		auth: {
 			user: process.env.SMTP_USER,
 			pass: process.env.SMTP_PASS
